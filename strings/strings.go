f332def7c27277faf8fbc283d7d15ebfdc5114d3
Guillermo Kardolus <kardolus@gmail.com>
2021-12-08 12:05:45 -0500

    Add string-methods

---
+++ strings/strings.go
@@ -0,0 +1,30 @@
+package strings
+
+import "strings"
+
+func Shorten(input string, size int) string {
+	if len(input) > size {
+		return input[:size]
+	}
+	return input
+}
+
+func StringToArray(input string) []string {
+	result := []string{input}
+
+	if strings.Contains(input, ",") {
+		return strings.Split(input, ",")
+	}
+
+	return result
+}
+
+func StringToInterface(args []string) []interface{} {
+	var result []interface{}
+
+	for _, arg := range args {
+		result = append(result, arg)
+	}
+
+	return result
+}
---
+++ strings/strings_test.go
@@ -0,0 +1,43 @@
+package strings_test
+
+import (
+	. "github.com/onsi/gomega"
+	"github.com/sclevine/spec"
+	"github.com/sclevine/spec/report"
+	"github.com/sidelight-labs/libc/strings"
+	"testing"
+)
+
+func TestUnitStrings(t *testing.T) {
+	spec.Run(t, "Time", testStrings, spec.Report(report.Terminal{}))
+}
+
+func testStrings(t *testing.T, when spec.G, it spec.S) {
+	it.Before(func() {
+		RegisterTestingT(t)
+	})
+
+	when("Shorten()", func() {
+		inputString := "0123456789"
+
+		it("returns the same string when its shorter than the maximum", func() {
+			result := strings.Shorten(inputString, 10)
+			Expect(result).To(HaveLen(10))
+			Expect(result).To(Equal(inputString))
+		})
+
+		it("returns a shortened string when it exceeds the maxiumum", func() {
+			result := strings.Shorten(inputString, 5)
+			Expect(result).To(HaveLen(5))
+			Expect(result).To(Equal("01234"))
+		})
+	})
+
+	when("StringToArray()", func() {
+		it("splits the string as expected", func() {
+			result := strings.StringToArray("a,b,c,d")
+			Expect(result).Should(HaveLen(4))
+			Expect(result[0]).Should(Equal("a"))
+		})
+	})
+}
