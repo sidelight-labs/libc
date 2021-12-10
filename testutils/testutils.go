ee6ad005f333ce783e3760ae3af0c01b0827c7e9
Guillermo Kardolus <kardolus@gmail.com>
2021-12-10 13:08:40 -0500

    Add support for vendored testutils

--- testutils/testutils.go
+++ testutils/testutils.go
@@ -1,4 +1,4 @@
-package testUtils
+package testutils
 
 import (
 	"fmt"
@@ -7,6 +7,7 @@ import (
 	"path"
 	"path/filepath"
 	"runtime"
+	"strings"
 )
 
 type MockUuidGenerator struct {
@@ -25,7 +26,16 @@ func (gen *MockUuidGenerator) GenerateMockUUID() string {
 func FileToString(fileName string) (string, error) {
 	_, thisFile, _, _ := runtime.Caller(0)
 
-	urlPath, err := filepath.Abs(path.Join(thisFile, "..", "..", "resources", "testdata", fileName))
+	var (
+		urlPath string
+		err     error
+	)
+	if strings.Contains(thisFile, "vendor") {
+		urlPath, err = filepath.Abs(path.Join(thisFile, "../../../../../..", "resources", "testdata", fileName))
+	} else {
+		urlPath, err = filepath.Abs(path.Join(thisFile, "../..", "resources", "testdata", fileName))
+	}
+
 	if err != nil {
 		return "", err
 	}
